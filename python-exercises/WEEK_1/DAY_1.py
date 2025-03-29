65cd6f3e80bdb60be14be3f3d25198b4fbcc5ed1
sitodaniel <kingdomdaniel@hotmail.com>
2025-03-29 20:50:21 +0100

    Update DAY_1.py

--- python-exercises/WEEK_1/DAY_1.py
+++ python-exercises/WEEK_1/DAY_1.py
@@ -1,1 +1,31 @@
+# Write a program that prints the message 'Hello, World' on the screen
+
+print("Hello, World")
+
+# Programs use variables to store information
+
+# 1. Create a variable in Python
+name = "Daniel"
+
+# 2. Use snake_case to name a variable with multiple words
+name_user = "DanielUser"
+
+# Variables can store different types of values like integer, float, string, and boolean.
+# 3. Create variables of each basic type and print a message combining them
+
+variable_one = 30               # Integer
+variable_two = 17.5             # Float
+variable_three = "Daniel"       # String
+variable_four = True            # Boolean
+
+# Print a combined message using an f-string
+print(f"These are the basic value types: {variable_one}, {variable_two}, {variable_three}, {variable_four}")
+
+""" What is an f-string in Python? """
+""" An f-string (short for formatted string) is a special 
+    type of string in Python that lets you insert variables 
+    directly into a string using curly braces {}. """
+
+name = "Daniel"
+print(f"Hello, {name}!")  # Output: Hello, Daniel!
 
